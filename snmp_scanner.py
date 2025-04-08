c8d770db40804932c584e62da4242b331b67ab59
crpMaster <croessner@web.de>
2025-04-08 13:23:01 +0200

    Switch to easysnmp for better Python 3.x compatibility

--- snmp_scanner.py
+++ snmp_scanner.py
@@ -1,4 +1,4 @@
-from pysnmp_lextudio.hlapi import *
+from easysnmp import Session
 import socket
 import time
 
@@ -7,10 +7,23 @@ class SNMPScanner:
         self.ip_address = ip_address
         self.port = port
         self.username = username
-        self.auth_protocol = self._get_auth_protocol(auth_protocol)
+        self.auth_protocol = auth_protocol.lower()
         self.auth_password = auth_password
-        self.priv_protocol = self._get_priv_protocol(priv_protocol)
+        self.priv_protocol = priv_protocol.lower()
         self.priv_password = priv_password
+        
+        # Create SNMP session
+        self.session = Session(
+            hostname=ip_address,
+            version=3,
+            security_level='authPriv',
+            security_username=username,
+            auth_protocol=self.auth_protocol,
+            auth_password=auth_password,
+            privacy_protocol=self.priv_protocol,
+            privacy_password=priv_password,
+            port=port
+        )
         
         # Common OIDs for system information
         self.system_oids = {
@@ -28,83 +41,20 @@ class SNMPScanner:
             'ifNumber': '1.3.6.1.2.1.2.1.0',
             'ifTable': '1.3.6.1.2.1.2.2'
         }
-        
-    def _get_auth_protocol(self, protocol_name):
-        auth_protocols = {
-            'MD5': usmHMACMD5AuthProtocol,
-            'SHA': usmHMACSHAAuthProtocol,
-            'SHA224': usmHMAC128SHA224AuthProtocol,
-            'SHA256': usmHMAC192SHA256AuthProtocol,
-            'SHA384': usmHMAC256SHA384AuthProtocol,
-            'SHA512': usmHMAC384SHA512AuthProtocol
-        }
-        return auth_protocols.get(protocol_name, usmHMACSHAAuthProtocol)
-    
-    def _get_priv_protocol(self, protocol_name):
-        priv_protocols = {
-            'DES': usmDESPrivProtocol,
-            '3DES': usm3DESEDEPrivProtocol,
-            'AES': usmAesCfb128Protocol,
-            'AES192': usmAesCfb192Protocol,
-            'AES256': usmAesCfb256Protocol
-        }
-        return priv_protocols.get(protocol_name, usmAesCfb128Protocol)
     
     def _snmp_get(self, oid):
         """Perform SNMP GET operation for a single OID"""
         try:
-            errorIndication, errorStatus, errorIndex, varBinds = next(
-                getCmd(
-                    SnmpEngine(),
-                    UsmUserData(
-                        self.username,
-                        authKey=self.auth_password,
-                        privKey=self.priv_password,
-                        authProtocol=self.auth_protocol,
-                        privProtocol=self.priv_protocol
-                    ),
-                    UdpTransportTarget((self.ip_address, self.port), timeout=2.0, retries=1),
-                    ContextData(),
-                    ObjectType(ObjectIdentity(oid))
-                )
-            )
-            
-            if errorIndication:
-                return {'error': str(errorIndication)}
-            elif errorStatus:
-                return {'error': f'{errorStatus.prettyPrint()} at {errorIndex and varBinds[int(errorIndex) - 1][0] or "?"}'}
-            else:
-                for varBind in varBinds:
-                    return {'oid': str(varBind[0]), 'value': str(varBind[1])}
+            result = self.session.get(oid)
+            return {'oid': result.oid, 'value': result.value}
         except Exception as e:
             return {'error': str(e)}
-        
+    
     def _snmp_walk(self, oid):
         """Perform SNMP WALK operation starting from a base OID"""
-        result = []
         try:
-            for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(
-                SnmpEngine(),
-                UsmUserData(
-                    self.username,
-                    authKey=self.auth_password,
-                    privKey=self.priv_password,
-                    authProtocol=self.auth_protocol,
-                    privProtocol=self.priv_protocol
-                ),
-                UdpTransportTarget((self.ip_address, self.port), timeout=2.0, retries=1),
-                ContextData(),
-                ObjectType(ObjectIdentity(oid)),
-                lexicographicMode=False
-            ):
-                if errorIndication:
-                    return {'error': str(errorIndication)}
-                elif errorStatus:
-                    return {'error': f'{errorStatus.prettyPrint()} at {errorIndex and varBinds[int(errorIndex) - 1][0] or "?"}'}
-                else:
-                    for varBind in varBinds:
-                        result.append({'oid': str(varBind[0]), 'value': str(varBind[1])})
-            return result
+            results = self.session.walk(oid)
+            return [{'oid': item.oid, 'value': item.value} for item in results]
         except Exception as e:
             return {'error': str(e)}
     
